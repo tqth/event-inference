a44eb9dc324946785fd94aa69b15e585889751b5
tqth <tqthinh2611@gmail.com>
2025-07-13 14:18:31 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
 
+try:
+    with open("README.md", "r", encoding="utf-8") as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = "Predict event from photo album using CLIP + RAM + Attention"
+
 setup(
     name='event_inference',
     version='0.1.0',
     description='Predict event from photo album using CLIP + RAM + Attention',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/tqth/event-inference',
     author='Quoc Thinh',
@@ -21,4 +27,9 @@ setup(
         'git+https://github.com/tqth/recognize-anything.git',
     ],
     python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
