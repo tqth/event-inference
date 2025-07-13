0c9688e05f597fe50eef3e9e77d88bf6dec0545f
tqth <tqthinh2611@gmail.com>
2025-07-13 14:56:05 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/tqth/event-inference",
     author="Quoc Thinh",
-    packages=find_packages(where="event_inference"),  # Tìm package trong event_inference
-    package_dir={"": "event_inference"},  # Định nghĩa gốc là event_inference
+    packages=find_packages(),  # Tìm package trong thư mục hiện tại (gốc)
+    package_dir={"": "."},  # Định nghĩa gốc package là thư mục hiện tại
     install_requires=[
         "torch>=1.7.0",
         "torchvision>=0.8.0",
